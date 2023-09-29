e6e39c09f307ba3cdb3fa971b9d83258656432c1
Rene Rendon <rene.rendon@hotmail.com>
2023-09-29 18:26:58 -0500

    Finished testing application Core. End of Section 3.

--- RoomBookingApp.Core.Tests/RoomBookingRequestProcessorTest.cs
+++ RoomBookingApp.Core.Tests/RoomBookingRequestProcessorTest.cs
@@ -1,4 +1,8 @@
-﻿using RoomBookingApp.Core.Models;
+﻿using Moq;
+using RoomBookingApp.Core.DataServices;
+using RoomBookingApp.Core.Domain;
+using RoomBookingApp.Core.Enums;
+using RoomBookingApp.Core.Models;
 using RoomBookingApp.Core.Processors;
 using Shouldly;
 
@@ -6,34 +10,138 @@ namespace RoomBookingApp.Core
 {
     public class RoomBookingRequestProcessorTest
 	{
+        private RoomBookingRequestProcessor _processor;
+        private RoomBookingRequest _request;
+        private Mock<IRoomBookingService> _roomBookingServiceMock;
+        private List<Room> _availableRooms;
+
+        public int Id { get; private set; }
+
+        public RoomBookingRequestProcessorTest()
+        { 
+            _request = new RoomBookingRequest
+            {
+                FullName = "Test Name",
+                Email = "test@request.com",
+                Date = new DateTime(2021, 10, 20)
+            };
+
+            _availableRooms = new List<Room>() { new Room { Id = 1 } };
+            _roomBookingServiceMock = new Mock<IRoomBookingService>();
+            _roomBookingServiceMock.Setup(q => q.GetAvailableRooms(_request.Date))
+                .Returns(_availableRooms);
+
+            _processor = new RoomBookingRequestProcessor(_roomBookingServiceMock.Object);
+        }
+
 		[Fact]
 		public void Should_Return_Room_Booking_Response_With_Request_Values()
 		{
 			// Arrange
-			var request = new RoomBookingRequest
-			{
-				FullName = "Test Name",
-				Email = "test@request.com",
-				Date = new DateTime(2021, 10, 20)
-			};
-
-			var processor = new RoomBookingRequestProcessor();
+			
 
 			// Act
 
-			RoomBookingResult result = processor.BookRoom(request);
+			RoomBookingResult result = _processor.BookRoom(_request);
 
 			// Assert
-			Assert.NotNull(result);
-			Assert.Equal(request.FullName, result.FullName);
-            Assert.Equal(request.FullName, result.FullName);
-            Assert.Equal(request.FullName, result.FullName);
-
             result.ShouldNotBeNull();
-			result.FullName.ShouldBe(request.FullName);
-            result.Email.ShouldBe(request.Email);
-            result.Date.ShouldBe(request.Date);
+			result.FullName.ShouldBe(_request.FullName);
+            result.Email.ShouldBe(_request.Email);
+            result.Date.ShouldBe(_request.Date);
         }
-	}
+
+		[Fact]
+		public void Should_Throw_Exception_For_Null_Request()
+		{
+            // Arrange
+
+			// Act
+
+			// Assert
+			var exception = Should.Throw<ArgumentNullException>(() => _processor.BookRoom(null));
+			exception.ParamName.ShouldBe("bookingRequest");
+        }
+
+        [Fact]
+        public void Should_Save_Room_Booking_Request()
+		{
+            // Arrange
+            RoomBooking savedBooking = null;
+            _roomBookingServiceMock.Setup(q => q.Save(It.IsAny<RoomBooking>()))
+                .Callback<RoomBooking>(booking =>
+                {
+                    savedBooking = booking;
+                });
+
+            // Act
+            _processor.BookRoom(_request);
+
+            // Assert
+            _roomBookingServiceMock.Verify(q => q.Save(It.IsAny<RoomBooking>()), Times.Once);
+            savedBooking.ShouldNotBeNull();
+            savedBooking.FullName.ShouldBe(_request.FullName);
+            savedBooking.Email.ShouldBe(_request.Email);
+            savedBooking.Date.ShouldBe(_request.Date);
+            savedBooking.RoomId.ShouldBe(_availableRooms.First().Id);
+        }
+
+        [Fact]
+        public void Should_Not_Save_Room_Booking_Request_If_None_Available()
+        {
+            // Arrange
+            _availableRooms.Clear();
+
+            // Act
+            _processor.BookRoom(_request);
+
+            // Assert
+            _roomBookingServiceMock.Verify(q => q.Save(It.IsAny<RoomBooking>()), Times.Never);
+        }
+
+        [Theory]
+        [InlineData(BookingResultFlag.Failure, false)]
+        [InlineData(BookingResultFlag.Success, true)]
+        public void Should_Return_SuccessFailure_Flag_In_Result(BookingResultFlag bookingSuccessFlag, bool isAvailable)
+        {
+            // Arrange
+            if (!isAvailable)
+            {
+                _availableRooms.Clear();
+            }
+
+            // Act
+            var result = _processor.BookRoom(_request);
+
+            // Assert
+            bookingSuccessFlag.ShouldBe(result.Flag);
+        }
+
+        [Theory]
+        [InlineData(1, true)]
+        [InlineData(null, false)]
+        public void Should_Return_RoomBookingId_In_Result(int? roomBookingId, bool isAvailable)
+        {
+            // Arrange
+            if (!isAvailable)
+            {
+                _availableRooms.Clear();
+            }
+            else
+            {
+                _roomBookingServiceMock.Setup(q => q.Save(It.IsAny<RoomBooking>()))
+                .Callback<RoomBooking>(booking =>
+                {
+                    booking.Id = roomBookingId.Value;
+                });
+            }
+
+            // Act
+            var result = _processor.BookRoom(_request);
+
+            // Assert
+            result.RoomBookingId.ShouldBe(roomBookingId);
+        }
+    }
 }
 
---
+++ RoomBookingApp.Core/Enums/BookingResultFlag.cs
@@ -0,0 +1,9 @@
+﻿namespace RoomBookingApp.Core.Enums
+{
+    public enum BookingResultFlag
+	{
+		Success,
+		Failure
+	}
+}
+
